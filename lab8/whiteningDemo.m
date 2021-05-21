e6a9f264995f569f8c1aae9cce2573510a570c09
Abz1521 <abz1521@gmail.com>
2021-05-21 12:29:28 -0500

    Whitening Filter 8.1

--- lab8/whiteningDemo.m
+++ lab8/whiteningDemo.m
@@ -1,6 +1,5 @@
 path = "testData.txt";
-data1 = load(path);
-  
+
 timeVec = data(:,1);
 dataVals  = data(:,2);
 
@@ -23,7 +22,24 @@ ovrlpSamp  = floor(ovrlp*sampFreq);
 % 
 figure;
 imagesc(T, F, abs(S)); axis xy;
-title('Spectrogram of testData');
+title('Spectrogram of Test Data');
 xlabel('Time (s)');
 ylabel('Frequency (Hz)');
 colorbar;
+
+[S1,F1,T1] = spectrogram(whitenedData, winLenSamp, ovrlpSamp, [], sampFreq);
+figure;
+imagesc(T1, F1, abs(S1)); axis xy;
+title('Spectrogram of Whittened Data');
+xlabel('Time (s)');
+ylabel('Frequency (Hz)');
+colorbar;
+
+figure;
+subplot(2,1,1), plot(timeVec, dataVals), title('Test Data');
+xlabel('Time (s)');
+ylabel('Amplitude');
+subplot(2,1,2), plot(timeVec, whitenedData), title('Whitened Data');
+xlabel('Time (s)');
+ylabel('Amplitude')
+
